c4122c9ec2d7a92b40510f1ce55267aed9141ce9
phuchit <ripzery@gmail.com>
2015-02-20 15:12:36 +0700

    card_info : if admin show all user else show only logged in user

--- custom/menus/card_info.php
+++ custom/menus/card_info.php
@@ -1,10 +1,14 @@
 <?php
     require_once('../includes/config.inc.php');
 
+    session_start();
+    
+    $userid = $_SESSION['uid'];
+    
     $link = mysqli_connect(DB_HOSTNAME, DB_USERNAME, DB_PASSWORD) or die("could not connect to host.");
     mysqli_select_db($link, DB_DATABASE)  or die("could not find db.");
 
-    $query = "SELECT * FROM cardinfo";
+    $query = $userid == 1 ? "SELECT * FROM cardinfo" : "SELECT * FROM cardinfo where uid = $userid";
 
     $result = mysqli_query($link, $query) or die("Data not found");
 ?>
@@ -51,12 +55,6 @@
 
                         </tbody>
                     </table>
-                </div>
-                <!-- /.table-responsive -->
-                <div class="well">
-                    <h4>DataTables Usage Information</h4>
-                    <p>DataTables is a very flexible, advanced tables plugin for jQuery. In SB Admin, we are using a specialized version of DataTables built for Bootstrap 3. We have also customized the table headings to use Font Awesome icons in place of images. For complete documentation on DataTables, visit their website at <a target="_blank" href="https://datatables.net/">https://datatables.net/</a>.</p>
-                    <a class="btn btn-default btn-lg btn-block" target="_blank" href="https://datatables.net/">View DataTables Documentation</a>
                 </div>
             </div>
             <!-- /.panel-body -->
