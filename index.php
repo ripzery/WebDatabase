8218be33534d2406987191d6202d5023e8bb7cc7
phuchit <ripzery@gmail.com>
2015-02-25 13:24:46 +0700

    fix initial page redirection

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
 <meta http-equiv="refresh" content="0;url=custom/index.html">
 <title>SB Admin 2</title>
 <script language="javascript">
-    window.location.href = "custom/index.html"
+    window.location.href = "custom/includes/login.inc.php"
 </script>
 </head>
 <body>
-Go to <a href="custom/index.html">/custom/index.html</a>
+Go to <a href="custom/includes/login.inc.php">/custom/includes/login.inc.php</a>
 </body>
 </html>
